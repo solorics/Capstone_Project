d16b12a50ed5f285ee62dc0bce5def9e75d7dadb
solomon opeyemi <opeyemisolomon906@gmail.com>
2022-11-30 12:07:23 +0100

    Create header section and it's content

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const totalBars = document.querySelectorAll('.menu-bar');
-/* eslint-disable no-unused-vars */
-function changeMenuIcon() {
-  for (let activeBar = 0;
-    activeBar < totalBars.length;
-    activeBar += 1) {
-    totalBars[activeBar].classList.toggle('active');
-  }
-}
+// const totalBars = document.querySelectorAll('.menu-bar');
+// const intro = document.getElementsByClassName('intro');
+// const spans = document.getElementsByTagName('span');
+// /* eslint-disable no-unused-vars */
+// function changeMenuIcon() {
+//   for (let activeBar = 0;
+//     activeBar < totalBars.length;
+//     activeBar += 1) {
+//     totalBars[activeBar].classList.toggle('active');
+//   }
+// }
