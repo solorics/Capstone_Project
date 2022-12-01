f07f7e2e54e7e70c77b299ff1f0bebdc82e60022
solomon opeyemi <opeyemisolomon906@gmail.com>
2022-12-01 12:10:17 +0100

    add mobile-menu to capstone

--- main.js
+++ main.js
@@ -9,3 +9,19 @@
 //     totalBars[activeBar].classList.toggle('active');
 //   }
 // }
+
+function openNav() {
+  document.getElementById('myNav').style.display = 'block';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.display = 'none';
+}
+
+document.querySelector('#open-nav').addEventListener('click', openNav);
+document.querySelector('#close-nav').addEventListener('click', closeNav);
+document.querySelectorAll('.mobile-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.getElementById('myNav').style.display = 'none';
+  });
+});
