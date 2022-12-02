21f773e6d4358a936156e39d2c24eccea797877a
solomon opeyemi <opeyemisolomon906@gmail.com>
2022-12-02 06:07:03 +0100

    Add secrion using dom manipulation

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// const totalBars = document.querySelectorAll('.menu-bar');
-// const intro = document.getElementsByClassName('intro');
-// const spans = document.getElementsByTagName('span');
-// /* eslint-disable no-unused-vars */
-// function changeMenuIcon() {
-//   for (let activeBar = 0;
-//     activeBar < totalBars.length;
-//     activeBar += 1) {
-//     totalBars[activeBar].classList.toggle('active');
-//   }
-// }
-
 function openNav() {
   document.getElementById('myNav').style.display = 'block';
 }
@@ -25,3 +13,61 @@ document.querySelectorAll('.mobile-link').forEach((link) => {
     document.getElementById('myNav').style.display = 'none';
   });
 });
+
+const guestSpeakers = document.querySelector('.guest-speakers');
+
+const speakerList = [
+  {
+    name: 'Beyonce',
+    description:
+      'Beyoncé Giselle Knowles-Carter is an American singer, songwriter, and actress.',
+    Image: './images/beyonce2.jpeg',
+    nickName: ['Queen Bey the queen of pop'],
+  },
+  {
+    name: 'Andre Romelle Young',
+    description:
+      'Andre Romelle Young, known professionally as Dr. Dre, is an American rapper and record producer.',
+    Image: './images/dr-dre1.jpeg',
+    nickName: ['Dr Dre'],
+  },
+  {
+    name: 'Céline Marie Claudette Dion',
+    description:
+      'Céline Marie Claudette Dion CC OQ is a Canadian singer. Noted for her powerful and technically skilled vocals,.',
+    Image: './images/celideon1.jpeg',
+    nickName: ['Celine Dion'],
+  },
+  {
+    name: 'Plácido Domingo',
+    description:
+      'José Plácido Domingo Embil is a Spanish opera singer, conductor, and arts administrator.',
+    Image: './images/domingo1.jpeg',
+    nickName: ['Domingo'],
+  },
+  {
+    name: 'Harry Edward Styles',
+    description:
+      'Harry Edward Styles is an English singer, songwriter, and actor.',
+    Image: './images/harry2.jpeg',
+    nickName: ['Harry Styles'],
+  },
+];
+
+guestSpeakers.innerHTML = '';
+for (let x = 0; x < speakerList.length; x += 1) {
+  guestSpeakers.innerHTML += `
+    <div class='speakers'>
+    <div class='speakers_image'>
+    <img src='${speakerList[x].Image}' alt='speaker 1' class='image1'/>
+    </div>
+    <div class='speakerDescription'><h3>${speakerList[x].name}</h3>
+    <div class='short_desc'><h4>${speakerList[x].nickName}</h4>
+    <span></span>
+    </div>
+    <div class='long_desc'>
+    <p>${speakerList[x].description}</p>
+    </div>
+    </div>
+  `;
+}
